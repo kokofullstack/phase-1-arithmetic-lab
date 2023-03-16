498e0b375615e56fbe1c82a201ef1c5462767861
Maung Ko Ko <mwkoko2020@gmail.com>
2023-03-16 02:46:11 -0600

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+multiply;
 
-// Write your code here
+const random = Math.round(Math.random() * 10);
+
+let num3 = 14;
+let num4 = 5;
+const mod = num3 % num4;
+mod;
+
+const max = Math.max(5,10,15,20);
